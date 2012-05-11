4d125f45271ae40806b06acae154b9d41d7558e9
Matthew Barbour <sanmadjack@masgau.org>
2012-05-11 11:12:37 -0500

    Cleanup and formatting

--- IniFileHandler.cs
+++ IniFileHandler.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
 using Collections;
-namespace Config
-{
-    public class IniFileHandler:  TwoKeyDictionary<String,String,String> 
-    {
+namespace Config {
+    public class IniFileHandler : TwoKeyDictionary<String, String, String> {
 
         public String getValue(String section, String value) {
             try {
                 return this.Get(section, value);
             } catch (KeyNotFoundException ex) {
-                throw new Exception("Could not find value in INI file",ex);
+                throw new Exception("Could not find value in INI file", ex);
             }
 
         }
@@ -23,19 +19,15 @@ namespace Config
         private Regex value_regex = new Regex(".*=.*");
 
         public IniFileHandler(FileInfo file) {
-            using (StreamReader sr = new StreamReader(file.FullName))
-            {
+            using (StreamReader sr = new StreamReader(file.FullName)) {
                 String line;
                 String section = null;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    if (header_regex.IsMatch(line))
-                    {
+                while ((line = sr.ReadLine()) != null) {
+                    if (header_regex.IsMatch(line)) {
                         char[] brackets = { '[', ']' };
                         section = line.Trim(brackets);
                     }
-                    if (value_regex.IsMatch(line))
-                    {
+                    if (value_regex.IsMatch(line)) {
                         char[] equals = { '=' };
                         string[] pair = line.Split(equals, 2);
                         this.Add(section, pair[0], pair[1]);
